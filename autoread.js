8bfcd03607486c6f4865971f3f8d10489cabf6ac
Emub <sodavansjuice@gmail.com>
2014-10-22 11:24:11 +0200

    Update autoread.js

--- autoread.js
+++ autoread.js
@@ -1,11 +1,13 @@
-$("input[type='text']").off().on("focus", function(){
-  var indexNum = 0,
-      children = $(this).parent().children("*:not(br)");
-  
-  for(var i = 0; i < children.length; i++){
-    if($(children[i]).attr("id") === $(this).attr("id")){
-      $(children[i - 1]).click();
-      break;
+$(function(){
+  $("input[type='text']").off().on("focus", function(){
+    var indexNum = 0,
+        children = $(this).parent().children("*:not(br)");
+    
+    for(var i = 0; i < children.length; i++){
+      if($(children[i]).attr("id") === $(this).attr("id")){
+        $(children[i - 1]).click();
+        break;
+      }
     }
-  }
+  });
 });
