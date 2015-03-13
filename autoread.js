fffdd7b86ecccdbd0d7a76a71a6704d2cf3e66d2
Emub <sodavansjuice@gmail.com>
2015-03-13 09:04:29 +0100

    Fix strongs not being visible on mac safari

--- autoread.js
+++ autoread.js
@@ -1,4 +1,6 @@
 $(function(){
+  
+  // Have the page auto-read
   $("input[type='text']").off().on("focus", function(){
     var indexNum = 0,
         children = $(this).parent().children("*:not(br)");
@@ -9,5 +11,12 @@ $(function(){
         break;
       }
     }
+  });
+  
+  // Fix strongs not being visible on mac safari
+  $("legend strong").css({
+    "font-size": "18px",
+    "font-weight": "900",
+    "color": "rgb(25, 0, 0)"
   });
 });
